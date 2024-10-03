e9bd00f82558e277b07c206eece1dd23cdd7d537
irfanzmohammed <irfanmuhammed877@gmail.com>
2024-10-03 22:04:07 +0530

    Sign In/Sign Up && Login/Header

---
+++ src/components/Body.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Login from './Login'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Browse from './Browse'
+const Body = () => {
+    const appRouter=createBrowserRouter([
+        {
+            path:"/",
+        element:<Login/>,
+        },
+        {
+            path:"/Browse",
+            element:<Browse/>,
+
+        }
+
+    ])
+
+  return (
+    <div>
+        <RouterProvider router={appRouter}/>
+    </div>
+  )
+}
+
+export default Body
